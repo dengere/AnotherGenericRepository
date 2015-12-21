b74105bef61f6278af1e62b003f21ce736407915
Nuri YILMAZ <nuri.yilmaz@outlook.com>
2015-12-21 15:27:05 +0200

    Simple fecth definition

---
+++ Persistent.Ef.Common/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Persistent.Ef.Common
+{
+    public interface IRepository<TEntity>
+        where TEntity : class, new()
+    {
+        TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+        TEntity Single(Expression<Func<TEntity, bool>> predicate);
+        TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predicate);
+        TEntity First(Expression<Func<TEntity, bool>> predicate);
+    }
+}
