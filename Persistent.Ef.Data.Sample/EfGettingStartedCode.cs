fcd48e8a51199aa4d2983e399cb75f03667680be
Nuri YILMAZ <nuri.yilmaz@outlook.com>
2015-12-23 16:33:29 +0200

    Fetch sample

---
+++ Persistent.Ef.Data.Sample/EfGettingStartedCode.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Data.Entity;
+using Persistent.Ef.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistent.Ef.Data.Sample
+{
+    public partial class Post
+    {
+        public int PostId { get; set; }
+        public int BlogId { get; set; }
+        public string Content { get; set; }
+        public string Title { get; set; }
+        public virtual Blog Blog { get; set; }
+    }
+
+    public partial class Blog
+    {
+        public Blog()
+        {
+            Post = new HashSet<Post>();
+        }
+
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public virtual ICollection<Post> Post { get; set; }
+    }
+
+    public partial class BloggingContext : DbContext, IDbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=.;Database=Blogging;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>(entity =>
+            {
+                entity.Property(e => e.Url).IsRequired();
+            });
+
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.HasOne(d => d.Blog).WithMany(p => p.Post).HasForeignKey(d => d.BlogId);
+            });
+        }
+
+        public IQueryable<TEntity> Queryable<TEntity>() where TEntity : class
+        {
+            return this.Set<TEntity>();
+        }
+
+        public virtual DbSet<Blog> Blog { get; set; }
+        public virtual DbSet<Post> Post { get; set; }
+    }
+}
---
+++ Persistent.Ef.Data.Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using Persistent.Ef.Common;
+using System;
+using System.Linq;
+
+namespace Persistent.Ef.Data.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BloggingContext context = new BloggingContext())
+            {
+                IRepository<Blog> blogRepo = new Repository<Blog>(context);
+                blogRepo.Fetch(p => true).ToList().ForEach(p => Console.WriteLine(p.Url));
+            }
+        }
+    }
+}
